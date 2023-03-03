075a96a12156fe8475087a88e7fc9f9fd3ad6754
Bilyana <bilyana.aneva@gmail.com>
2023-03-03 19:37:02 +0200

    featureA from B

--- src/newPackageB/ClassOfB.java
+++ src/newPackageB/ClassOfB.java
@@ -3,5 +3,6 @@ package newPackageB;
 public class ClassOfB {
     public static void main(String[] args) {
         System.out.println("third push from B");
+        System.out.println("4th push to branch feature from B");
     }
 }
